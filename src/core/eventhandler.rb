4916254a58fb5c3a3b7f3c91b001cd8e18b62004
Alexander Kitaev <alex.kitaev58@gmail.com>
2014-06-30 12:20:14 -0400

    Work on Event Handler

---
+++ src/core/eventhandler.rb
@@ -0,0 +1,35 @@
+module MegaText
+	class Event
+	end
+	class ProgramStartEvent < Event
+	end
+	class FileOpenEvent < Event
+		def initialize(filename)
+			@filename = filename
+		end
+	end
+	class FileCloseEvent < Event
+		def initialize(filename)
+			@filename = filename
+		end
+	end
+	class ProgramCloseEvent < Event
+	end
+	class EventHandler
+		@eventListeners = Hash.new
+		def self.addEventType(eventClass)
+			return if @eventListeners.has_key? eventClass
+			@eventListeners << {eventClass => []}
+		end
+		def self.addEventListener(eventClass, proc)
+			return if not @eventListeners.has_key? eventClass
+			@eventListeners[eventClass] << proc
+		end
+		def self.signalEvent(event)
+			return if not @eventListeners.has_key? event.class
+			for proc in @eventListeners[event.class]
+				proc.call(event)
+			end
+		end
+	end
+end
